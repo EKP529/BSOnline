17208bc604b2ffa9aea3764f4b4f2f121a070093
EKP529 <ekpaupulaire@gmail.com>
2023-06-03 13:00:58 -0500

    did front end prep/adjustments for login functionality

--- public/index.js
+++ public/index.js
@@ -1,5 +1,17 @@
 localStorage.setItem('loggedIn', 'f');
 
+(async () => {
+  const username = localStorage.getItem('userName');
+  if (username) {
+    document.querySelector('#playerName').textContent = username;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
 let usernameEl;
 let passwordEL;
 
@@ -9,15 +21,62 @@ function checkInputs() {
     return (usernameEl.value !== '') && (passwordEL.value !== '');
 
 }
-function login() {
-    if (checkInputs()) {
-        localStorage.setItem("userName", usernameEl.value);
-        localStorage.setItem("password", passwordEL.value);
-        localStorage.setItem('loggedIn', 't');
-        window.location.href = "lobby.html";
-    }
+async function login() {
+  loginOrCreate(`/api/auth/login`);
 }
 
+async function register() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  if (checkInputs()) {
+    const username = usernameEl.value;
+    const password = passwordEL.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    
+    if (response.ok) {
+      localStorage.setItem('username', username);
+      localStorage.setItem('loggedIn', 't');
+      play();
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
+  }
+}
+function play() {
+  window.location.href = 'lobby.html';
+}
+function logout() {
+  localStorage.removeItem('userName');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+async function getUser(username) {
+  // See if we have a user with the given username.
+  const response = await fetch(`/api/user/${username}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+  return null;
+}
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
 function displayQuote(data) {
     fetch('https://api.quotable.io/random')
       .then((response) => response.json())
